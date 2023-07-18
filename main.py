6ce90985c3215543784701fa5a2aa541f1e98bf4
雪見だいふく <100012108+mochidukiyukimi@users.noreply.github.com>
2023-07-18 21:54:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -180,6 +180,7 @@ def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
     if check_cokie(yuki):
         response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
         return template("home.html",{"request": request})
+    print(check_cokie(yuki))
     return redirect("/word")
 
 @app.get('/watch', response_class=HTMLResponse)
